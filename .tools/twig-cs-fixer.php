7826b7dfcf812b9b22dec4bc66f87953fa13105f
Oleg Kasyanov <oleg@batumi.org>
2026-06-30 16:18:56 +0400

    Updated tools configuration

--- .tools/twig-cs-fixer.php
+++ .tools/twig-cs-fixer.php
@@ -9,7 +9,7 @@ $ruleset = new TwigCsFixer\Ruleset\Ruleset();
 $ruleset->addStandard(new TwigCsFixer\Standard\TwigCsFixer());
 
 $config = new TwigCsFixer\Config\Config();
-$config->setCacheFile(__DIR__.'/.cache/twig-cs-fixer.cache');
+$config->setCacheFile(dirname(__DIR__) . '/.cache/twig-cs-fixer.cache');
 $config->setFinder($finder);
 $config->setRuleset($ruleset);
 
